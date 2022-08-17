8ea9abc82a32d2a5903f8f05c636cf4ab0f884c4
pablogomez1234 <pablogomez26995@gmail.com>
2022-08-16 23:01:26 -0300

    optimizando el proyecto final

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-const listaSuper = [];
-
-let entrada;
-do {
-  entrada = prompt('Ingrese un producto de la libreria. Ingrese FIN para terminar');
-  if (entrada != 'FIN') {
-    listaSuper.push(entrada);
-  }
-} while (entrada != 'FIN');
 
 
-alert('La lista tiene ' + listaSuper.length + ' productos.');
+Swal.fire({
+  title: 'hola!',
+  text: 'Bienvenido',
+  icon: 'success',
+  confirmButtonText: 'OK'
+  
+});
 
-alert('La lista tiene:\n' + listaSuper.join('\n'));
 
-
-
-class Producto {
+ class Producto {
   constructor(precio, cantidad) {
     this.precio = parseFloat(precio);
     this.cantidad = parseInt(cantidad);
-  }
-}
+  } }
+
+
